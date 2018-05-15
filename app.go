3f09fb03a3b0c1994ead08314592319e0487890c
xmdom <dom@xmdevelopments.com>
2018-05-16 11:04:30 +1200

    build 107

--- app.go
+++ app.go
@@ -2,13 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/DeanThompson/ginpprof"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/gzip"
+	"github.com/gin-gonic/gin"
 )
 
 var now = time.Now()
-var election = time.Date(2018, time.June, 1, 0, 0, 0, 0, time.UTC)
+var election = time.Date(2018, time.May, 25, 16, 0, 0, 0, time.UTC)
 
 func main() {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
+	//router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"POST", "GET", "OPTIONS"},
+		AllowHeaders:    []string{"Authorization", "Access-Control-Allow-Origin", "Content-Type, Accept"},
+	}))
+
+	router.GET("/ping", func(ginContext *gin.Context) {
+		ginContext.String(200, "pong")
+	})
+
+	memGroup := router.Group("/user/")
+	{
+		memGroup.GET("/list", userList)
+	}
+
+	exitGroup := router.Group("/exit/")
+	{
+		exitGroup.GET("/date", exitDate)
+	}
+
+	HTTP := &http.Server{
+		Addr:           ":8046",
+		Handler:        router,
+		ReadTimeout:    100000 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	ginpprof.Wrap(router)
+
+	err := HTTP.ListenAndServe()
+	//bugsnag.Notify(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func userList(g *gin.Context) {
+
+	userList := []string{"Dom", "Matias", "Elan", "Mary", "Jacques"}
+	g.JSON(200, userList)
+}
+
+func exitDate(g *gin.Context) {
 	//get duration between election date and now
 	tillElection := election.Sub(now)
 	//get duration in nanoseconds
@@ -25,6 +80,6 @@ func main() {
 	days := hours / 24
 	hoursLeft := hours % 24
 
-	fmt.Printf("\nHow long until the great exit of 2018?\n\n%v Days %v Hours %v Minutes %v Seconds\n\n", days, hoursLeft, minutes, seconds)
+	g.JSON(200, fmt.Sprintf("%v Days %v Hours %v Minutes %v Seconds", days, hoursLeft, minutes, seconds))
 
 }
